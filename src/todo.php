7cf24ffaf88976e5f6afe976d6450464d845338b
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-24 10:20:52 +0530

    rectified the errors

--- src/todo.php
+++ src/todo.php
@@ -1,78 +1,102 @@
 <?php
-	session_start();
-	if(!ISSET($_SESSION['todo'])){
-		$_SESSION['todo']=array();
-    }
-    include('function.php');
-    if(isset($_GET['action'])){
-        $task = $_GET['task'];
-        switch ($_GET['task']){
-            case "add":
-                add($task);
-                break;
-            case "delete":
-                delete($_GET['id']);
-                break;
-            case "update":
-                update($_GET['task'], $_GET['id']);
-                break;
-            case "check":
-                complete($_GET['id']);
-                break;
-        }
-    }
-    ?>
+session_start();
+if (!isset($_SESSION['todo'])) {
+
+    $_SESSION['todo'] = array();
+}
+include('function.php');
+if (isset($_GET['action'])) {
+
+    $tsk = $_GET['tsk'];
 
 
+    switch ($_GET['action']) {
+
+
+        case "add":
+            add($tsk);
+            break;
+
+        case 'delete':
+            delete($_GET['id']);
+            break;
+
+        case "update":
+            update($_GET['tsk'], $_GET['idd']);
+            break;
+
+        case "check":
+            complete($_GET['id']);
+            break;
+    }
+}
+
+?>
 
 <html>
-    <head>
-        <title>TODO List</title>
-        <link href="style.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
-        
-        <div class="container">
-            <h2>TODO LIST</h2>
-            <h3>Add Item</h3>
-            <p>
-                <form action="" method="GET">
-                    <?php
-                    if($_GET['action']=='edit'){
-                        ?>
-                <input id="new-task" type="text" name="task" value=<?php gettsk($_GET['id']); ?>>
-                <input type="hidden" name="id" value=<?php echo $_GET['id'];?>>
-                <input type="submit" name="add" value="Update"/>
 
-            </p>
+<head>
+    <title>TODO List</title>
+    <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h2>TODO LIST</h2>
+        <h3>Add Item</h3>
+        <p>
+
+        <form action="" method="GET">
+
             <?php
-                    }
-                    else {
-                        ?>
-                        <input id="new-task" type="text" name="task" value=<?php if($_GET['action']=="edit"){
-                            ?>
-                            <?php
-                            gettsk($_GET['id']);
-                            ?><?php } else {
-                                ?>
-                                 ""
-                                 <?php } 
-                                ?>
-                                 <input type="submit" name="add" value="Update"/>
-                            <?php } ?>
-                        
-                            </form>
+            if ($_GET['action'] == 'edit') {
 
-            <h3>Todo</h3>
-            <ul id="incomplete-tasks">
-                <?php display(); ?>
-            </ul>
-    
-            <h3>Completed</h3>
-            <ul id="completed-tasks">
-           <?php display2(); ?>
+            ?>
+
+                <input id="new-task" type="text" name="tsk" value=<?php gettsk($_GET['id']) ?>>>
+
+
+                <input type="hidden" name="idd" value=<?php echo $_GET['id']; ?>>
+
+                <input type="submit" name="action" value="update">>
+
+
+            <?php } else { ?>
+                <input id="new-task" type="text" name="tsk" value=<?php
+                                                                    if ($_GET['action'] == 'edit') {
+                                                                    ?>
+                                                                    <?php
+
+                                                                        gettsk($_GET['id']);
+        
+                                                                        ?> 
+                                                                        <?php 
+                                                                    } else 
+                                                                    { ?>
+                                                                    "" <?php 
+                                                                } 
+                                                                ?>>
+                <input type="submit" name="action" value="add">
+
+            <?php } ?>
+
+
+        </form>
+        </p>
+
+        <h3>Todo</h3>
+        <ul id="incomplete-tasks">
+
+            <?php display(); ?>
         </ul>
-        </div>
-    
-    </body>
+
+        <h3>Completed</h3>
+        <ul id="completed-tasks">
+
+            <?php display2() ?>
+        </ul>
+    </div>
+
+</body>
+
 </html>
