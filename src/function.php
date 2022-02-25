98e35757fae7392c7eb0d1569db1c2f4c5f8d939
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-25 13:13:57 +0530

    PHP toDO

--- src/function.php
+++ src/function.php
@@ -1,80 +1,54 @@
 <?php
 session_start();
-function add($tsk)
-{
-  $num = count($_SESSION['todo']) + 1;
-  $task = array(
-    "id" => $num,
-    "name" => $tsk,
-    "check" => "f",
+//add items in todo list
+if(isset($_POST['addBtn'])){
+    if(isset($_SESSION['todo'])){
+        array_push($_SESSION['todo'],$_POST['input']);
+    }else{
+        $_SESSION['todo']=array($_POST['input']);
 
-  );
-  array_push($_SESSION['todo'], $task);
-}
-
-function display()
-{
-  $list = "";
-  foreach ($_SESSION['todo'] as $k => $arr) {
-
-    if ($arr['check'] == "f") {
-
-      $list .= "<form action='' method=GET><li><h4> <input type=hidden name=id value=" . $k . "><input type=checkbox name=action value=check onChange=this.form.submit()><label><h3>" . $arr['name'] . "</h3><a class='action' &nbsp&nbsp&nbsp&nbsp&nbsp href=todo.php?id=" . $k . "&action=edit&val=" . $arr['name'] . ">Edit&nbsp&nbsp&nbsp&nbsp</a><a class='action' href=todo.php?id=" . $k . "&action=delete</a>Delete </h4></li></form>";
     }
-  }
-  echo $list;
-}
-
-function display2()
-{
-  $list = "";
-  foreach ($_SESSION['todo'] as $k => $arr) {
-
-    if ($arr['check'] == "t") {
-
-      $list .= "<li><input type=checkbox checked><label>" . $arr['name'] . "</label><input type=text><button class=edit>Edit</button><button class=delete>Delete</button></li>";
     }
-  }
-  echo $list;
-}
-function delete($id)
-{
-  foreach ($_SESSION['todo'] as $k => $arr) {
-    if ($k == $id) {
-      unset($_SESSION['todo'][$k]);
+
+//edit button
+if(isset($_POST['editButton'])){
+    $_SESSION['myVar']=$_SESSION['todo'][$_POST['mVal']];
+    unset($_SESSION['todo'][$_POST['mVal']]);
+    if(isset($_SESSION['myVar'])){
+        $_SESSION['count']=1;
+    }else{
+        $_SESSION['count']=0;
     }
-  }
 }
-
-function gettsk($id)
-{
-  foreach ($_SESSION['todo'] as $k => $arr) {
-
-    if ($k == $id) {
-      echo $arr['name'];
+if(isset($_POST['addBtn'])){
+    $_SESSION['count']=0;
+}
+//delete button
+if(isset($_POST['deleteBtn'])){
+    unset($_SESSION['todo'][$_POST['mVal']]);
+}
+// add item in checkbox
+if(isset($_POST['check'])){
+    $_SESSION['val2']=$_SESSION['todo'][$_POST['mVal']];
+    unset($_SESSION['todo'][$_POST['mVal']]);
+    if(isset($_SESSION['checkBox'])){
+    array_push($_SESSION['checkBox'],$_SESSION['val2']); 
+}else{
+    $_SESSION['checkBox']=array($_SESSION['val2']);
+}
+}
+//edit and delete
+if(isset($_POST['editButton2'])){
+    $_SESSION['myVar']=$_SESSION['checkBox'][$_POST['mVal']];
+    unset($_SESSION['checkBox'][$_POST['mVal']]);
+    if(isset($_SESSION['myVar'])){
+        $_SESSION['count']=1;
+    }else{
+        $_SESSION['count']=0;
     }
-  }
 }
-
-function update($tsk, $id)
-{
-  foreach ($_SESSION['todo'] as $k => $arr) {
-
-    if ($k == $id) {
-      delete($id);
-      add($tsk);
-    }
-  }
+if(isset($_POST['deleteBtn2'])){
+    unset($_SESSION['checkBox'][$_POST['mVal']]);
 }
-function complete($id)
-{
-
-  foreach ($_SESSION['todo'] as $k => $arr) {
-
-    if ($k == $id) {
-
-
-      $_SESSION['todo'][$k]['check'] = "t";
-    }
-  }
-}
+header('location: todo.php');
+?>
--- src/todo.php
+++ src/todo.php
@@ -1,102 +1,53 @@
 <?php
 session_start();
-if (!isset($_SESSION['todo'])) {
-
-    $_SESSION['todo'] = array();
-}
-include('function.php');
-if (isset($_GET['action'])) {
-
-    $tsk = $_GET['tsk'];
-
-
-    switch ($_GET['action']) {
-
-
-        case "add":
-            add($tsk);
-            break;
-
-        case 'delete':
-            delete($_GET['id']);
-            break;
-
-        case "update":
-            update($_GET['tsk'], $_GET['idd']);
-            break;
-
-        case "check":
-            complete($_GET['id']);
-            break;
-    }
-}
-
 ?>
 
 <html>
-
-<head>
-    <title>TODO List</title>
-    <link href="style.css" type="text/css" rel="stylesheet">
-</head>
-
-<body>
-    <div class="container">
-        <h2>TODO LIST</h2>
-        <h3>Add Item</h3>
-        <p>
-
-        <form action="" method="GET">
-
-            <?php
-            if ($_GET['action'] == 'edit') {
-
-            ?>
-
-                <input id="new-task" type="text" name="tsk" value=<?php gettsk($_GET['id']) ?>>>
-
-
-                <input type="hidden" name="idd" value=<?php echo $_GET['id']; ?>>
-
-                <input type="submit" name="action" value="update">>
-
-
-            <?php } else { ?>
-                <input id="new-task" type="text" name="tsk" value=<?php
-                                                                    if ($_GET['action'] == 'edit') {
-                                                                    ?>
-                                                                    <?php
-
-                                                                        gettsk($_GET['id']);
+    <head>
+        <title>TODO List</title>
+        <link href="style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <form action="function.php" method="POST">
+            <h2>TODO LIST</h2>
+            <h3>Add Item</h3>
+            <input id="new-task" type="text" name="input" value="<?php echo $_SESSION['myVar']; ?>"><button name="addBtn">
+                <?php
+                if($_SESSION['count']==0){
+                    ?>
+                    Add
+                <?php
+                }else{
+                    ?>
+                    Update
+                <?php
+                $_SESSION['count']=0;
+                }
+                ?></button>
+            </form>
+    
+            <h3>Todo</h3>
+            <ul id="incomplete-tasks">
+                <?php if(isset($_SESSION['todo'])){
+            foreach($_SESSION['todo'] as $key => $value){
+                echo '<form action="function.php" method="POST"><li><input type="checkbox" onchange="this.form.submit()" name="check"><label>.'.$value.'</label><input type="text"><button class="edit" name="editButton">Edit</button><button class="delete" name="deleteBtn">Delete</button></li><input type="text" hidden name="mVal" value="'.$key.'"></form>';
+            }
+        } ?>
+            </ul>
+    
+            <h3>Completed</h3>
+            <ul id="completed-tasks">
+                 <?php 
+                if(isset($_SESSION['checkBox'])){
+                    foreach($_SESSION['checkBox'] as $key1=>$value1){
+                        echo '<form action="function.php" method="POST"><li><input type="checkbox" checked><label>.'.$value1.'</label><input type="text"><button class="edit" name="editButton2">Edit</button><button class="delete" name="deleteBtn2">Delete</button></li><input type="text" hidden name="mVal" value="'.$key1.'"></form>';
+                    }
+                }
+                ?> 
+                </ul>
+        </div>
         
-                                                                        ?> 
-                                                                        <?php 
-                                                                    } else 
-                                                                    { ?>
-                                                                    "" <?php 
-                                                                } 
-                                                                ?>>
-                <input type="submit" name="action" value="add">
 
-            <?php } ?>
-
-
-        </form>
-        </p>
-
-        <h3>Todo</h3>
-        <ul id="incomplete-tasks">
-
-            <?php display(); ?>
-        </ul>
-
-        <h3>Completed</h3>
-        <ul id="completed-tasks">
-
-            <?php display2() ?>
-        </ul>
-    </div>
-
-</body>
-
+    </body>
 </html>
